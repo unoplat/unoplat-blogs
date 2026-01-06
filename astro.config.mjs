a36d1c2eb2610690331a4ab5cb51537acfac7be7
JayGhiya <ghiya6548@gmail.com>
2026-01-06 12:16:34 +0530

    feat: first blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,23 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import photosuite from 'photosuite';
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blogs.unoplat.io',
-	integrations: [mdx({ remarkPlugins: [remarkReadingTime] }), sitemap(), react()],
+	integrations: [
+		mdx({ remarkPlugins: [remarkReadingTime] }),
+		sitemap(),
+		react(),
+		photosuite({
+			scope: '.prose',
+			glightbox: true,
+			imageAlts: true,
+		}),
+	],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 	},
